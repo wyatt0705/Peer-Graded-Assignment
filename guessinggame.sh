f14ad37bffa5ba0f52ce329f2257466fcafac997
Yang Wang <wangyangtj752@gmail.com>
2020-08-16 22:51:08 -0500

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+# File: guessinggame
+
+function guess {
+	local correct_answer = $(ls . | wc -w)
+
+	while true
+	do
+		echo "Enter your guess of number of files in current directory)"
+		read number
+		if [ $number -lt $correct_answer ]
+		then
+			echo "Sorry, but your guess is less than the actual number"
+		elif [ $number -gt $correct_answer ]
+		then
+			echo "Sorry, your guess is more than the actual number"
+		else
+			echo "Great! You got it!"
+		break
+		fi
+	done
+}
+echo "Guess the number of files in current directory"
+
